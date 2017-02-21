df7fc90e03140c747ac0471cb3634d84a2bcfc09
JayMvrt1017 <jvy.mvrt@icloud.com>
2017-02-21 00:17:26 -0500

    Assigment 2 Added

---
+++ week2/sketch.js
@@ -0,0 +1,60 @@
+function setup(){
+    createCanvas(1080, 2000);
+    
+    var pink = "#d0a1a8";
+    var gray = "#a9a9a9";
+    var nocolor = "white";
+    var a;
+    a = 175
+    var b;
+    b = 220
+    var c;
+    c = 350
+    var d;
+    d = 20
+    var e;
+    e = 200
+    var f;
+    f = 150
+    var g;
+    g = 150
+    var h;
+    h = 550
+    var i;
+    i = 30
+    var j;
+    j = 20
+    var k;
+    k = 5
+    var l;
+    l = 8
+    var m;
+    m = 243
+    var n; 
+    n = 260
+    var o;
+    o = 13
+    var p;
+    p = 630
+    var q;
+    q = 25
+    
+    fill(pink)    
+    rect(a, g, c, h, d);
+    
+    rect(a, e, c, d);
+    
+    rect(a, p, c, q);
+    
+    fill(gray)
+    ellipse(b, a, i);
+    
+    fill(nocolor)
+    ellipse(b, a, j);
+    
+    ellipse(b, a, k);
+   
+    ellipse(m, a, l);
+    
+    ellipse(n, a, o);
+}
