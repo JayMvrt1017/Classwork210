345067f7d245a9decd149e70d124c8d5dab75e9d
JayMvrt1017 <jvy.mvrt@icloud.com>
2017-02-28 17:53:36 -0500

    week4 sketch.js edited

--- week4/sketch.js
+++ week4/sketch.js
@@ -1,12 +1,21 @@
 // system variable 
-var x;
+var x = 10;
+var y = 50;
 function setup() {
     createCanvas(640, 480);
     background ('skyblue');
 }
 
 
-function draw() {
-    rect(50, 50, 50, 50);
 
+function draw() {
+    background ('skyblue');
+    rect(x, y, 50, 50);
+    
+
+    if (x < width-100){
+        x += 10;
+    } else { y += 10;
+    }
+    
 }
