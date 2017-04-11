6f75d10700ffa0d4e4fc4db740953fed6d2df872
JayMvrt1017 <jvy.mvrt@icloud.com>
2017-04-11 07:12:12 -0400

    Window Assignment #5 Complete

--- week5/sketch.js
+++ week5/sketch.js
@@ -1,7 +1,6 @@
 // windows
 function setup() { 
    createCanvas(640, 680);
-    
 }
 
 function draw() {
@@ -9,37 +8,37 @@ function draw() {
     
     var windowWidth = width/4; // window width
     var windowHeight = height/2; // window height
-    var top = color(76, 60, 112); //Top-Rect-Color
-    var topMid = color (39, 83, 93); //topMid-Rect-Color
-    var bottoMid = color(100, 55, 33); //bottoMid-Rect-color
-	var bottom = color(47, 76, 11); //bottom-Rect-Color
+		var top = color(76, 60, 112); //Top-Rect-Color
+		var topMid = color (39, 83, 93); //topMid-Rect-Color
+		var bottoMid = color(100, 55, 33); //bottoMid-Rect-color
+		var bottom = color(47, 76, 11); //bottom-Rect-Color
 	
 			for (var x = windowWidth/6; x < width; x+=windowWidth) {
 				for (var y = windowHeight/8; y < height; y+=windowHeight) {
 				
 				
 			fill("black");
-			rect (x, y, windowWidth/2, 210);
+			rect (x-5, y-5, windowWidth/2, 210);
 	
 		fill(top);
 		rect (x, y, windowWidth/8, windowHeight/8);
-		rect (x*2, y, windowWidth/8, windowHeight/8);
-		rect (x*3, y, windowWidth/8, windowHeight/8);
+		rect (x+25, y, windowWidth/8, windowHeight/8);
+		rect (x+50, y, windowWidth/8, windowHeight/8);
 	
 		fill(topMid);
+		rect(x+25, y+50, windowWidth/8, windowHeight/8);
+		rect(x+50, y+50, windowWidth/8, windowHeight/8);
 		rect(x, y+50, windowWidth/8, windowHeight/8);
-		rect(x*2, y+50, windowWidth/8, windowHeight/8);
-		rect(x*3, y+50, windowWidth/8, windowHeight/8);
 	
 		fill(bottoMid);
 		rect(x, y+100, windowWidth/8, windowHeight/8);
-		rect(x*2, y+100, windowWidth/8, windowHeight/8);
-		rect(x*3, y+100, windowWidth/8, windowHeight/8);
+		rect(x+25, y+100, windowWidth/8, windowHeight/8);
+		rect(x+50, y+100, windowWidth/8, windowHeight/8);
 	
 		fill(bottom);
 		rect(x, y+150, windowWidth/8, windowHeight/8);
-		rect(x*2, y+150, windowWidth/8, windowHeight/8);
-		rect(x*3, y+150, windowWidth/8, windowHeight/8);
+		rect(x+25, y+150, windowWidth/8, windowHeight/8);
+		rect(x+50, y+150, windowWidth/8, windowHeight/8);
 
 				}
 	
