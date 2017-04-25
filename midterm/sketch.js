f2d674415ce3f8c2098d78a0bd9fca2a149e2f59
JayMvrt1017 <jvy.mvrt@icloud.com>
2017-04-25 12:29:24 -0400

    Midterm Finished

--- midterm/sketch.js
+++ midterm/sketch.js
@@ -1,16 +1,80 @@
+// midterm - generative pattern
+
 function setup() {
-	createCanvas(640, 640);
-	background("white");
-	
-	var columns = 6;
-	var rows = 6;
-	var w = width/columns;
-	var h = height/rows;
-	
-	for (var x = 53; x < width; x += w) {
-		for (var y = 60; y < height/3; y += h) {
-			ellipse (x, y, 100, 100);
-      triangle (0, 0, 53, 60, 0, 0);
-		}
-	}
+    createCanvas(640, 640);
+    background(255); 
+    
+   noStroke();
+    
+    var numberOfRectangles = 300;
+		var rows = 20;
+    var columns = numberOfRectangles / rows;
+    
+    var w = width / columns;
+    var h = height / rows;
+
+    for (var x = 0; x <= width; x += w) {
+        for (var y = 0; y <= height; y += h) {
+            
+            var r, g, b;
+            var tempW = w;
+            
+            
+            // rain
+            if (x + y+50 < height/3) {
+                // random rain blue color
+                r = random(200, 255);         
+                g = random(235, 255);
+                b = random(0); 
+								ellipse (0, 0, 400);
+
+            } else if (y > height * 3/4) {
+							
+                // water ellipses color
+                r = random(100, 100);
+                g = random(100, 120);
+                b = random(120, 255);
+							
+                // water ellipses size
+                w = h = random(15, 25);
+            } else {
+							
+                // random green grass color
+                r = random(245, 255);
+                g = random(155, 255);
+                b = random(0, 255);
+            }
+            
+            // all of the color fills
+            fill(r, g, b);            
+            
+            // Mini Blue River
+            if (y > height * 3/4) {
+							
+            // Bottom Blue Water
+                rect(x, y, w+20, h);
+   
+            } else {
+							
+            // rain & grass
+                rect(x, y+200, w/2, h);
+            }
+					
+            w = tempW;
+					
+						ellipse(300, 100, 50);
+						ellipse(320, 100, 50);
+						ellipse(340, 100, 50);
+						ellipse(360, 100, 50);
+						ellipse(380, 100, 50);
+					
+						ellipse(460, 100, 50);
+						ellipse(480, 100, 50);
+						ellipse(500, 100, 50);
+						ellipse(520, 100, 50);
+						ellipse(540, 100, 50);
+					
+  
+        }
+    }
 }
--- week6/sketch.js
+++ week6/sketch.js
@@ -1,7 +1,29 @@
-// windows
+// Animation
 function setup() { 
-   createCanvas(640, 480);
+   createCanvas(640, 680);
 }
 
+x = 50
+y = 10
+var ellipseSize = 150
+var ellipseEye = 20
+
 function draw() {
+    
+    background("black");
+    fill("yellow");
+    ellipse (x + width/4, y + height/2.5, ellipseSize);
+    
+    fill("black");
+    ellipse (x + width/4 + 20, y + height/3, ellipseEye);
+    
+    fill("black");
+    triangle (x + 150, y + width/2.3, x + 200 + 85, y + height/2.2, x + 200 + 90, y + width/2.7 );
+    
+    x = x + 5
+    
+    if (x > width) {
+        x = -100;
+    } 
+    
 }
